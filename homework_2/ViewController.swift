5885a2230522f24bed435dbffe32f72c81433014
ian rust <ibrust777@gmail.com>
2019-12-31 19:56:19 -0500

    Initial Commit

---
+++ homework_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_2
+//
+//  Created by Field Employee on 12/31/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
